8b8849a74321fd88bd9333e3f5b6c312e6cbd337
Serhat Ay <seraksa18@gmail.com>
2020-06-28 12:32:54 +0200

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk commit from local repo");
+		System.out.println("ikinci commit from local repo");
 		
 		
 		
